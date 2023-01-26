64dd195e526444aa4d38ba04c67e494ae1871dfe
_MRZT721010_ <69120140+mrzt721010@users.noreply.github.com>
2023-01-26 18:15:39 +0330

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const doParticles = true;
+const getWidth = () => { 
+    return Math.max(
+        document.body.scrollWidth,
+        document.documentElement.scrollWidth,
+        document.body.offsetWidth,
+        document.documentElement.offsetWidth,
+        document.documentElement.clientWidth
+    );
+};
+
+if (doParticles) {
+    if (getWidth() < 400) $.firefly({
+        minPixel: 1,
+        maxPixel: 2,
+        total: 20
+    });
+    else $.firefly({
+        minPixel: 1,
+        maxPixel: 3,
+        total: 40
+    });
+}
+
